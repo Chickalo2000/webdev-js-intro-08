0acde4c00ccb5c2b70422d6c5f6e9b749ec7ed9f
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-19 23:27:53 -0500

    experimented some

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let grades = [85, 90, 78, 92, 88];
+let sum = 0;
+for (let i = 0; i < grades.length; i++) {
+    sum += grades[i];
+}
+
+let average = sum / grades.length;
+console.log(average);
+
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] += 10;
+}
+console.log(numbers);
+
+let numbers = [10, 20, 30, 40, 50];
+for (let number of numbers) {
+    console.log(number);
+} 
+
+
+let grades = [85, 90, 78, 92, 88];
+let sum = 0;
+for (let grade of grades) {
+    sum += grade;
+}
+
+let average = sum / grades.length;
+console.log(average);
+
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(number) {
+    console.log(number);
+});
+
+*/
